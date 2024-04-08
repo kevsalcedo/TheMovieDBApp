2cd793e3ff2e27d2c424dfe3a245046780e4774a
kevsalcedo <kevsalcedom@gmail.com>
2024-04-08 16:11:06 -0500

    update repository and add isLading to search bar

--- lib/infrastructure/models/moviedb/movie_moviedb.dart
+++ lib/infrastructure/models/moviedb/movie_moviedb.dart
@@ -1,70 +1,70 @@
 class MovieMovieDB {
-    bool adult;
-    String backdropPath;
-    List<int> genreIds;
-    int id;
-    String originalLanguage;
-    String originalTitle;
-    String overview;
-    double popularity;
-    String posterPath;
-    DateTime releaseDate;
-    String title;
-    bool video;
-    double voteAverage;
-    int voteCount;
+  bool adult;
+  String backdropPath;
+  List<int> genreIds;
+  int id;
+  String originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String posterPath;
+  DateTime? releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
 
-    MovieMovieDB({
-        required this.adult,
-        required this.backdropPath,
-        required this.genreIds,
-        required this.id,
-        required this.originalLanguage,
-        required this.originalTitle,
-        required this.overview,
-        required this.popularity,
-        required this.posterPath,
-        required this.releaseDate,
-        required this.title,
-        required this.video,
-        required this.voteAverage,
-        required this.voteCount,
-    });
+  MovieMovieDB({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
 
-    MovieMovieDB copyWith({
-        bool? adult,
-        String? backdropPath,
-        List<int>? genreIds,
-        int? id,
-        String? originalLanguage,
-        String? originalTitle,
-        String? overview,
-        double? popularity,
-        String? posterPath,
-        DateTime? releaseDate,
-        String? title,
-        bool? video,
-        double? voteAverage,
-        int? voteCount,
-    }) => 
-        MovieMovieDB(
-            adult: adult ?? this.adult,
-            backdropPath: backdropPath ?? this.backdropPath,
-            genreIds: genreIds ?? this.genreIds,
-            id: id ?? this.id,
-            originalLanguage: originalLanguage ?? this.originalLanguage,
-            originalTitle: originalTitle ?? this.originalTitle,
-            overview: overview ?? this.overview,
-            popularity: popularity ?? this.popularity,
-            posterPath: posterPath ?? this.posterPath,
-            releaseDate: releaseDate ?? this.releaseDate,
-            title: title ?? this.title,
-            video: video ?? this.video,
-            voteAverage: voteAverage ?? this.voteAverage,
-            voteCount: voteCount ?? this.voteCount,
-        );
+  MovieMovieDB copyWith({
+    bool? adult,
+    String? backdropPath,
+    List<int>? genreIds,
+    int? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    double? popularity,
+    String? posterPath,
+    DateTime? releaseDate,
+    String? title,
+    bool? video,
+    double? voteAverage,
+    int? voteCount,
+  }) =>
+      MovieMovieDB(
+        adult: adult ?? this.adult,
+        backdropPath: backdropPath ?? this.backdropPath,
+        genreIds: genreIds ?? this.genreIds,
+        id: id ?? this.id,
+        originalLanguage: originalLanguage ?? this.originalLanguage,
+        originalTitle: originalTitle ?? this.originalTitle,
+        overview: overview ?? this.overview,
+        popularity: popularity ?? this.popularity,
+        posterPath: posterPath ?? this.posterPath,
+        releaseDate: releaseDate ?? this.releaseDate,
+        title: title ?? this.title,
+        video: video ?? this.video,
+        voteAverage: voteAverage ?? this.voteAverage,
+        voteCount: voteCount ?? this.voteCount,
+      );
 
-    factory MovieMovieDB.fromJson(Map<String, dynamic> json) => MovieMovieDB(
+  factory MovieMovieDB.fromJson(Map<String, dynamic> json) => MovieMovieDB(
         adult: json["adult"] ?? false,
         backdropPath: json["backdrop_path"] ?? "",
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
@@ -74,14 +74,17 @@ class MovieMovieDB {
         overview: json["overview"] ?? "",
         popularity: json["popularity"]?.toDouble(),
         posterPath: json["poster_path"] ?? "",
-        releaseDate: DateTime.parse(json["release_date"]),
+        releaseDate: json["release_date"] != null && json["release_date"].toString().isNotEmpty
+            ? DateTime.parse(json["release_date"])
+            : null,
+        //releaseDate: json["release_date"] ?? "",
         title: json["title"],
         video: json["video"],
         voteAverage: json["vote_average"]?.toDouble(),
         voteCount: json["vote_count"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "adult": adult,
         "backdrop_path": backdropPath,
         "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
@@ -91,10 +94,12 @@ class MovieMovieDB {
         "overview": overview,
         "popularity": popularity,
         "poster_path": posterPath,
-        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+        "release_date": releaseDate != null
+            ? "${releaseDate!.year.toString().padLeft(4, '0')}-${releaseDate!.month.toString().padLeft(2, '0')}-${releaseDate!.day.toString().padLeft(2, '0')}"
+            : null,
         "title": title,
         "video": video,
         "vote_average": voteAverage,
         "vote_count": voteCount,
-    };
+      };
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,37 +1,56 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movies_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
     return SafeArea(
-      bottom: false,
-      child: Padding(
-        padding: const EdgeInsets.symmetric( horizontal: 10 ),
-        child: SizedBox(
-          width: double.infinity,
-          child: Row(
-            children: [
-              Icon( Icons.movie_outlined, color: colors.primary ),
-              const SizedBox( width: 5 ),
-              Text('Cinemapedia', style: titleStyle ),
-      
-              const Spacer(),
-      
-              IconButton(onPressed: (){
-      
-              }, 
-              icon: const Icon(Icons.search)
-              )
-            ],
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(Icons.movie_outlined, color: colors.primary),
+                const SizedBox(width: 5),
+                Text('Cinemapedia', style: titleStyle),
+                const Spacer(),
+                IconButton(
+                    onPressed: () {
+                      final movieRepository = ref.read(movieRepositoryProvider);
+
+                      final searchedMovies = ref.read(searchedMoviesProvider);
+                      final searchQuery = ref.read(searchQueryProvider);
+
+                      showSearch<Movie?>(
+                              query: searchQuery,
+                              context: context,
+                              delegate: SearchMoviesDelegate(
+                                  initialMovies: searchedMovies,
+                                  searchMovies: ref
+                                      .read(searchedMoviesProvider.notifier)
+                                      .searchMoviesByQuery))
+                          .then((movie) {
+                        if (movie == null) return;
+
+                        context.push('/movie/${movie.id}');
+                      });
+                    },
+                    icon: const Icon(Icons.search))
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
